03818a82881fe9f495148a30d6bd0858f50a4b46
Tezla-Lee <tezla.lee@gmail.com>
2020-07-29 16:10:34 +0900

    modify Homework #2

--- Homework2/RingCounter.java
+++ Homework2/RingCounter.java
@@ -11,14 +11,19 @@ public class RingCounter {
         System.out.print("numBit 입력 : ");
         int numBits = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("numCount 입력 : ");
+        System.out.print("numCount 입력 : ");
         int numCount = scanner.nextInt();
+        int zeroNum = numBits - (numCount % numBits) - 1;
 
+        System.out.print("현재 링카운터의 값 : 0b");
+        for ( int i = 0; i < zeroNum; i++){
+            System.out.print("0");
+        }
+        System.out.printf("%s\n", Integer.toBinaryString(1 << numCount % numBits));
 
-        System.out.printf("0b%s\n", Integer.toBinaryString(1 << numCount % numBits));
+//        System.out.printf("0b%s%s\n", ,Integer.toBinaryString(1 << numCount % numBits));
 
-        //numBits - (numCount % numBits)
-        System.out.println("현재 링카운터의 값 : " + (1 << numCount % numBits));
+        System.out.println("현재 링카운터의 10진수 값 : " + (1 << numCount % numBits));
 
     }
 }
